4d9676f7318212ef00e1e82fe3f367abea773f33
AntoineMulpas <mulpas.antoine@gmail.com>
2023-10-05 12:14:04 +0200

    Implementing mapper for address.

--- src/main/java/com/medilabo/microservice_patient/utils/AddressMapper.java
+++ src/main/java/com/medilabo/microservice_patient/utils/AddressMapper.java
@@ -5,6 +5,18 @@ import com.medilabo.microservice_patient.models.Address;
 public class AddressMapper {
 
     public static Address map(Address initial, Address update) {
+        if (update.getAddress() != null) {
+            initial.setAddress(update.getAddress());
+        }
+
+        if (update.getCity() != null) {
+            initial.setCity(update.getCity());
+        }
+
+        if (update.getZip() != null) {
+            initial.setZip(update.getZip());
+        }
+
         return initial;
     }
 
