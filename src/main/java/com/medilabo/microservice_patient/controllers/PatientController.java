cc3c49c998e915b8059b8916f4da272ed9084379
AntoineMulpas <mulpas.antoine@gmail.com>
2023-10-05 12:09:43 +0200

    Implementing CRUD in controller.

---
+++ src/main/java/com/medilabo/microservice_patient/controllers/PatientController.java
@@ -0,0 +1,69 @@
+package com.medilabo.microservice_patient.controllers;
+
+import com.medilabo.microservice_patient.exceptions.PatientDoesNotExistException;
+import com.medilabo.microservice_patient.models.Patient;
+import com.medilabo.microservice_patient.services.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/patient")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Patient> add(
+            @RequestBody Patient patient) {
+        try {
+            Patient added = patientService.add(patient);
+            return ResponseEntity.ok().body(added);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> findById(Long id) {
+        try {
+            Patient patient = patientService.findById(id);
+            return ResponseEntity.ok().body(patient);
+        } catch (PatientDoesNotExistException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<String> deleteById(
+            @PathVariable Long id
+    ) {
+        try {
+            patientService.deleteById(id);
+            return ResponseEntity.ok().body("Patient deleted with success.");
+        } catch (PatientDoesNotExistException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<Patient> updateById(
+            @PathVariable Long id,
+            @RequestBody Patient patient) {
+        try {
+            Patient updateById = patientService.updateById(id, patient);
+            return ResponseEntity.ok().body(updateById);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+
+}
