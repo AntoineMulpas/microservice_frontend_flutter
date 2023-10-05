3a724f7c6bf109a8ced49d4d007fe6aefb9dba0f
AntoineMulpas <mulpas.antoine@gmail.com>
2023-10-05 11:57:48 +0200

    Implementing custom exception. Implementing mapper. Implementing CRUD methods.

--- src/main/java/com/medilabo/microservice_patient/services/AddressService.java
+++ src/main/java/com/medilabo/microservice_patient/services/AddressService.java
@@ -1,9 +1,15 @@
 package com.medilabo.microservice_patient.services;
 
+import com.medilabo.microservice_patient.exceptions.AddressDoesNotExistsException;
+import com.medilabo.microservice_patient.models.Address;
 import com.medilabo.microservice_patient.repositories.AddressRepository;
+import com.medilabo.microservice_patient.utils.AddressMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -15,4 +21,39 @@ public class AddressService {
     public AddressService(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
     }
+
+    public Address add(Address address) {
+        return addressRepository.save(address);
+    }
+
+    public Address findById(Long id) throws AddressDoesNotExistsException {
+        return addressExistById(id);
+    }
+
+    public Address updateById(Long id, Address address) throws AddressDoesNotExistsException {
+        Address addressExistById = addressExistById(id);
+        return addressRepository.save(AddressMapper.map(addressExistById, address));
+    }
+
+    public void deleteById(Long id) throws AddressDoesNotExistsException {
+        Optional <Address> optionalAddress = addressRepository.findById(id);
+        if (optionalAddress.isPresent()) {
+            addressRepository.deleteById(id);
+        }
+        throw new AddressDoesNotExistsException("Patient does not exist for id: " + id);
+    }
+
+    public List <Address> findAll() {
+        return addressRepository.findAll();
+    }
+
+    private Address addressExistById(Long id) throws AddressDoesNotExistsException {
+        Optional <Address> address = addressRepository.findById(id);
+        if (address.isPresent()) {
+            return address.get();
+        }
+        throw new AddressDoesNotExistsException("Address does not exist for id: " + id);
+    }
+
+
 }
---
+++ src/main/java/com/medilabo/microservice_patient/utils/AddressMapper.java
@@ -0,0 +1,11 @@
+package com.medilabo.microservice_patient.utils;
+
+import com.medilabo.microservice_patient.models.Address;
+
+public class AddressMapper {
+
+    public static Address map(Address initial, Address update) {
+        return initial;
+    }
+
+}
